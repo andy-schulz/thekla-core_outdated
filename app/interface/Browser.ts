6137e3529ee6cccf3f0f2336a6be26b108bfdf43
Andy Schulz <andy.schulz@test-steps.de>
2018-11-07 21:05:11 +0100

    add BrowserFactory for browser creation depending on different Frameworks

--- app/interface/Browser.ts
+++ app/interface/Browser.ts
@@ -1,6 +1,21 @@
 import {WebFinder} from "./WebElements";
+import {Config} from "./Config";
+import {BrowserWdjs} from "../src/wdjs/BrowserWdjs";
+import {Condition, ConditionElement} from "./Condition";
 
 export interface Browser extends WebFinder{
     get(url: string): Promise<any>;
     quit(): Promise<void>;
+    wait(condition: Condition, timeout?: number, errorMessage?: string): Promise<string>;
+}
+
+export class BrowserFactory {
+
+    public static create(config: Config, framework: string): Promise<Browser> {
+        if(framework == "wdjs") {
+            return BrowserWdjs.create(config);
+        } else {
+            throw Promise.reject(`Error: Framework ${framework} not implemented`);
+        }
+    }
 }
