67b2b6189a20f125a380cd865be2ef37e584c20f
Andy Schulz <andy.schulz@test-steps.de>
2018-12-01 12:29:15 +0100

    add UntilElement and Utils to Index

--- app/index.ts
+++ app/index.ts
@@ -10,13 +10,15 @@ export {BrowserFactory} from "./driver/lib/BrowserFactory";
 export {By}             from "./driver/lib/Locator";
 export {Key}            from "./driver/lib/Key";
 export {until}          from "./driver/lib/Condition";
+export {UntilElement}   from "./driver/lib/ElementConditions";
+
+export {Utils}  from "./driver/utils/Utils";
 
 /**
  *
  * Request Wrapper
  *
  */
-
 export {Get}    from "./screenplay/rest/actions/Get"
 export {Post}   from "./screenplay/rest/actions/Post"
 export {Delete} from "./screenplay/rest/actions/Delete"
@@ -25,8 +27,8 @@ export {Delete} from "./screenplay/rest/actions/Delete"
  * screenplay Elements
  */
 export {element, all, frame}                                         from "./screenplay/web/SppWebElements";
-export {SppWebElementFinder, SppWebElementListFinder}         from "./screenplay/web/SppWebElements";
-export {Actor}                                                from "./screenplay/Actor";
+export {SppWebElementFinder, SppWebElementListFinder}                from "./screenplay/web/SppWebElements";
+export {Actor}                                                       from "./screenplay/Actor";
 
 // Abilities
 export {Ability}                                                from "./screenplay/lib/abilities/Ability";
