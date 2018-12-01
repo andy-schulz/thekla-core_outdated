0cd54ce6397a2aed43c5ad79897d5233dfa0b816
Andy Schulz <andy.schulz@test-steps.de>
2018-12-01 12:28:26 +0100

    bugfix spp element type for all method

--- app/test/PageObjects/GoogleSearch/GoogleSearch.ts
+++ app/test/PageObjects/GoogleSearch/GoogleSearch.ts
@@ -1,8 +1,8 @@
-import {all, element, SppWebElementFinder} from "../../../screenplay/web/SppWebElements";
-import {By}                                from "../../../index";
+import {all, element, SppWebElementFinder, SppWebElementListFinder} from "../../../screenplay/web/SppWebElements";
+import {By}                                                         from "../../../index";
 
 export class GoogleSearch {
     public static searchField: SppWebElementFinder = element(By.css("[name='q']"));
-    public static searchFieldAll: SppWebElementFinder = all(By.css("[name='q']"));
+    public static searchFieldAll: SppWebElementListFinder = all(By.css("[name='q']"));
 }
 
