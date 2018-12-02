d38622eb18fd29076923d521647220888da154d7
Andy Schulz <andy.schulz@test-steps.de>
2018-12-02 10:56:48 +0100

    update examples to use a page object

--- app/test/screenplay/doc_spp_examples_spec.ts
+++ app/test/screenplay/doc_spp_examples_spec.ts
@@ -2,6 +2,20 @@ import {
     Actor, BrowserFactory, BrowseTheWeb, By, Click, Config, element, Enter, Key, Navigate, See, Text, UntilElement
 } from "../..";
 
+class GooglePgo {
+    // define your elements in a page object
+    public static searchField = element(By.css("[name='q']"))
+        .shallWait(UntilElement.isVisible().forAsLongAs(5000))
+        .called("The Google search field");
+
+    public static submitSearch = element(By.css(".FPdoLc [name='btnK']"))
+        .called("The Google Submit Search button on the main Page");
+
+    public static calculatorInput = element(By.css("#cwos"))
+        .called("Google calculator input field")
+        .shallWait(UntilElement.isVisible().forAsLongAs(5000));
+}
+
 describe('Using Google Search to find an online calculator', () => {
     const conf: Config = {
         browserName: "chrome",
@@ -13,30 +27,17 @@ describe('Using Google Search to find an online calculator', () => {
         // and give him the ability to browse the web using a browser of your choice
         philipp.whoCan(BrowseTheWeb.using(BrowserFactory.create(conf)));
 
-        // define your elements preferably in a separate class like a page object
-        const searchField = element(By.css("[name='q']"))
-            .shallWait(UntilElement.isVisible().forAsLongAs(5000))
-            .called("The Google search field");
-
-        const submitSearch = element(By.css(".FPdoLc [name='btnK']"))
-            .called("The Google Submit Search button on the main Page");
-
-        const calculatorInput = element(By.css("#cwos"))
-            .called("Google calculator input field")
-            .shallWait(UntilElement.isVisible().forAsLongAs(5000));
-
         // using the jasmine matcher for our checks on the UI
         const match = (expected: string) => (actual: string) => expect(actual).toEqual(expected);
 
         it('the google calculator should be loaded', async () => {
             return philipp.attemptsTo(
                 Navigate.to("http://www.google.com"),
-                Enter.value("calculator").into(searchField),
-                Enter.value(Key.TAB).into(searchField),
-                Click.on(submitSearch),
-                See.if(Text.of(calculatorInput)).fulfills(match("0"))
+                Enter.value("calculator").into(GooglePgo.searchField),
+                Enter.value(Key.TAB).into(GooglePgo.searchField),
+                Click.on(GooglePgo.submitSearch),
+                See.if(Text.of(GooglePgo.calculatorInput)).fulfills(match("0"))
             )
-
         }, 20000);
     });
 
