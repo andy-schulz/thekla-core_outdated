b7282f7d6e74c58a2cf5d82dc289666717b758bb
Andy Schulz <andy.schulz@test-steps.de>
2019-03-11 18:54:56 +0100

    add repeater to See Matcher

--- app/screenplay/lib/matcher/See.ts
+++ app/screenplay/lib/matcher/See.ts
@@ -3,22 +3,71 @@ import {Question}         from "./Question";
 import {Oracle}           from "../actions/Activities";
 
 export class See<U> implements Oracle {
-    matcher: (value: U) => any;
+    matcher: (value: U) => boolean | Promise<boolean>;
+    private repeater: number = 1;
+    private ms: number = 1000;
 
     static if <T>(question: Question<T>): See<T> {
         return new See(question)
     }
 
-    fulfills(matcher: (text: U) => any): See<U> {
+    fulfills(matcher: (text: U) => boolean | Promise<boolean>): See<U> {
         this.matcher = matcher;
         return this;
     }
+
+    repeatFor(times: number, interval: number = 1000): See<U> {
+
+        if(times < 1 || times > 1000)
+            throw new Error(`The repeat value should be between 1 and 1000. But its: ${times}`);
+
+        if(interval < 0 || interval > 60000)
+            throw new Error(`The interval value should be between 1 and 60000 ms (1 minute). But its: ${interval}`);
+
+        this.repeater = times;
+        this.ms = interval;
+        return this;
+    }
+
 
     constructor(
         private question: Question<U>
     ) {}
 
     async performAs(actor: AnswersQuestions): Promise<void> {
-        return await (this.matcher(await actor.toAnswer(this.question)))
+
+        const loop = async (counter: number): Promise<boolean> => {
+            const nextLoop = () => {
+                return new Promise(resolve => setTimeout(resolve, this.ms))
+                    .then(() => {
+                        return loop(counter -1);
+                    });
+            };
+
+            if(counter < 1)
+                return Promise.resolve((this.matcher(await actor.toAnswer(this.question))));
+
+            try {
+                let promise = Promise.resolve((this.matcher(await actor.toAnswer(this.question))));
+            } catch (e) {
+                return nextLoop();
+            }
+
+
+            return Promise.resolve((this.matcher(await actor.toAnswer(this.question))))
+                .then((matched: boolean) => {
+                    if(!matched) {
+                        return nextLoop();
+                    } else {
+                        return matched;
+                    }
+                })
+                .catch((e) => {
+                    return nextLoop();
+
+                })
+        };
+
+        return await loop(this.repeater - 1).then((a) => {return});
     }
 }
--- app/test/screenplay/spp_questions_spec.ts
+++ app/test/screenplay/spp_questions_spec.ts
@@ -1,12 +1,14 @@
-import {SeleniumConfig} from "../../config/SeleniumConfig";
-import {BrowserFactory} from "../../driver/lib/BrowserFactory";
-import {By}             from "../../driver/lib/Locator";
-import {Actor}          from "../../screenplay/Actor";
-import {See}            from "../../screenplay/lib/matcher/See";
-import {BrowseTheWeb}   from "../../screenplay/web/abilities/BrowseTheWeb";
-import {Navigate}       from "../../screenplay/web/actions/Navigate";
-import {Attribute}      from "../../screenplay/web/matcher/questions/Attribute";
-import {element}        from "../../screenplay/web/SppWebElements";
+import {SeleniumConfig}               from "../../config/SeleniumConfig";
+import {BrowserFactory}               from "../../driver/lib/BrowserFactory";
+import {By}                           from "../../driver/lib/Locator";
+import {Actor}                        from "../../screenplay/Actor";
+import {See}                          from "../../screenplay/lib/matcher/See";
+import {BrowseTheWeb}                 from "../../screenplay/web/abilities/BrowseTheWeb";
+import {Navigate}                     from "../../screenplay/web/actions/Navigate";
+import {Attribute}                    from "../../screenplay/web/matcher/questions/Attribute";
+import {element, SppWebElementFinder} from "../../screenplay/web/SppWebElements";
+import {strictEqual} from "assert";
+
 
 let config: SeleniumConfig = {
     seleniumServerAddress: `http://localhost:4444/wd/hub`,
@@ -20,6 +22,8 @@ let config: SeleniumConfig = {
     }
 };
 
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+
 describe('Using', () => {
 
     afterAll(async () => {
@@ -29,26 +33,160 @@ describe('Using', () => {
 
     describe('the attribute question', () => {
         let John: Actor = Actor.named("John");
+        let button: SppWebElementFinder;
+        let matcher: (text: string) => boolean;
 
         beforeAll(async () => {
             const browser = await BrowserFactory.create(config);
-            John.can(BrowseTheWeb.using(browser))
+            John.can(BrowseTheWeb.using(browser));
+
+            button = element(By.css("[data-test-id='button']"))
+                .called("the danger button");
+
+            matcher = (text: string): boolean => {
+                expect(text).toEqual("<button data-test-id=\"button\" class=\"btn btn-danger\">Danger!</button>");
+                // can return true
+                return true;
+            };
+        });
+
+        it('"See": it should throw an error when the repeater value is out of bounds. ' +
+            '- (test case id: e0e5340d-294f-4795-ab31-08a3a71c58a4)', async () => {
+            try {
+                await John.attemptsTo(
+                    Navigate.to("/"),
+                    See.if(Attribute.of(button).called("outerHTML"))
+                        .fulfills(matcher)
+                        .repeatFor(0,1000)
+                )
+            } catch (e) {
+                expect(e.toString()).toContain("The repeat value should be between 1 and 1000. But its: 0")
+            }
+
+            try {
+                await John.attemptsTo(
+                    Navigate.to("/"),
+                    See.if(Attribute.of(button).called("outerHTML"))
+                        .fulfills(matcher)
+                        .repeatFor(-1,1000)
+                )
+            } catch (e) {
+                expect(e.toString()).toContain("The repeat value should be between 1 and 1000. But its: -1")
+            }
+
+            try {
+                await John.attemptsTo(
+                    Navigate.to("/"),
+                    See.if(Attribute.of(button).called("outerHTML"))
+                        .fulfills(matcher)
+                        .repeatFor(1001,1000)
+                )
+            } catch (e) {
+                expect(e.toString()).toContain("The repeat value should be between 1 and 1000. But its: 1001")
+            }
+        });
+
+        it('"See": it should throw an error when the interval value is out of bounds. ' +
+            '- (test case id: 0676e29a-529e-474c-8413-da4c29897ab5)', async () => {
+            try {
+                await John.attemptsTo(
+                    Navigate.to("/"),
+                    See.if(Attribute.of(button).called("outerHTML"))
+                        .fulfills(matcher)
+                        .repeatFor(2,-1)
+                )
+            } catch (e) {
+                expect(e.toString()).toContain("The interval value should be between 1 and 60000 ms (1 minute). But its: -1")
+            }
+
+            try {
+                await John.attemptsTo(
+                    Navigate.to("/"),
+                    See.if(Attribute.of(button).called("outerHTML"))
+                        .fulfills(matcher)
+                        .repeatFor(6,60001)
+                )
+            } catch (e) {
+                expect(e.toString()).toContain("The interval value should be between 1 and 60000 ms (1 minute). But its: 60001")
+            }
         });
 
         it('for retrieving the data-test-id attribute should return the attribute value ' +
             '- (test case id: 6458382e-d95d-49b6-972c-56fa68bede94)', async () => {
-
-            const button = element(By.css("[data-test-id='button']"))
-                        .called("the danger button");
-
-            const matcher = (text: string) => {
-                expect(text).toEqual("<button data-test-id=\"button\" class=\"btn btn-danger\">Danger!</button>")
-            };
-
             await John.attemptsTo(
                 Navigate.to("/"),
                 See.if(Attribute.of(button).called("outerHTML")).fulfills(matcher)
             )
+        });
+
+        it('should poll the status until the button appears ' +
+            '- (test case id: 0d5c98b2-9975-4c30-a81f-5a8ef862a0aa)', async () => {
+            const delayedButton =
+                    element(By.css("[data-test-id='AppearButtonBy5000']"))
+                        .called("button which appears after 5 seconds");
+
+            const matcher = (text: string): boolean => {
+                strictEqual(text,"<button data-test-id=\"AppearButtonBy5000\" class=\"btn btn-info\">Appeared after 5 seconds</button>");
+                return true;
+            };
+
+            await John.attemptsTo(
+                Navigate.to("/redirect"),
+                See.if(Attribute.of(delayedButton)
+                    .called("outerHTML"))
+                    .fulfills(matcher)
+                    .repeatFor(12,1000)
+            )
+        });
+
+        it('should poll the status and throws an error after the given tries ' +
+            '- (test case id: b1afa0cd-bb66-432a-b6d3-755b422d6506)', async () => {
+            const delayedButton =
+                element(By.css("[data-test-id='AppearButtonBy5000']"))
+                    .called("button which appears after 5 seconds");
+
+            const matcher = (text: string): boolean => {
+                strictEqual(text,"<button data-test-id=\"AppearButtonBy5000\" class=\"btn btn-info\">Appeared after 5 seconds</button>");
+                return true;
+            };
+
+            try {
+                await John.attemptsTo(
+                    Navigate.to("/redirect"),
+                    See.if(Attribute.of(delayedButton)
+                        .called("outerHTML"))
+                        .fulfills(matcher)
+                        .repeatFor(5,1000)
+                );
+                expect(true).toBeFalsy("call should have thrown an error. But it did not.");
+            } catch (e) {
+                expect(e.toString()).toContain("No Element found: 'button which appears after 5 seconds' selected by: >>byCss: [data-test-id='AppearButtonBy5000']<<")
+            }
+
+        });
+
+        it('should throw an error on first try when the element is not found ' +
+            '- (test case id: 29bbbf6f-4741-48fb-9c44-7ecec23d1240)', async () => {
+            const delayedButton =
+                element(By.css("[data-test-id='AppearButtonBy5000']"))
+                    .called("button which appears after 5 seconds");
+
+            const matcher = (text: string): boolean => {
+                return expect(text).toEqual("<button data-test-id=\"AppearButtonBy5000\" class=\"btn btn-info\">Appeared after 5 seconds</button>");
+            };
+
+            try {
+                await John.attemptsTo(
+                    Navigate.to("/redirect"),
+                    See.if(Attribute.of(delayedButton)
+                        .called("outerHTML"))
+                        .fulfills(matcher)
+                );
+                expect(true).toBeFalsy("call should have thrown an error. But it did not.")
+
+            } catch (e) {
+                expect(e.toString()).toContain("No Element found: 'button which appears after 5 seconds' selected by: >>byCss: [data-test-id='AppearButtonBy5000']<<")
+            }
 
         });
     });
