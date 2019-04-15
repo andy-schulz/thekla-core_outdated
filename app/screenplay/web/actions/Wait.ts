c9fe07de2c5c26dc7fc6545cd0263d5e23064fe4
Andy Schulz <andy.schulz@test-steps.de>
2019-04-15 06:11:17 +0200

    add additional documentation

--- app/screenplay/web/actions/Wait.ts
+++ app/screenplay/web/actions/Wait.ts
@@ -1,8 +1,11 @@
+/**
+ * Wait until a condition is met on a given element
+ */
 import {stepDetails}           from "../../lib/decorators/StepDecorators";
 import {BrowseTheWeb}          from "../abilities/BrowseTheWeb";
 import {SppWebElementFinder}   from "../SppWebElements";
 import {Interaction}           from "../../lib/actions/Activities";
-import {Actor, UsesAbilities}  from "../../Actor";
+import {UsesAbilities}  from "../../Actor";
 import {getLogger}             from "@log4js-node/log4js-api"
 import {UntilElementCondition} from "../../../driver/lib/ElementConditions";
 
@@ -10,22 +13,14 @@ export class Wait implements Interaction {
     private logger = getLogger("Wait");
     private condition: UntilElementCondition;
 
-    public static for(waitCondition: SppWebElementFinder) {
-        return new Wait(waitCondition);
-    }
-
-    public andCheck(condition: UntilElementCondition) {
-        this.condition = condition;
-        return this;
-    }
-
-    constructor(private waitCondition: SppWebElementFinder | number) {}
-
-    @stepDetails<UsesAbilities>(`wait for '<<condition>>'.`)
+    /**
+     * @ignore
+     */
+    @stepDetails<UsesAbilities>(`wait for '<<awaitingElement>>' and check '<<condition>>'.`)
     performAs(actor: UsesAbilities): Promise<void> {
 
         return new Promise((resolve, reject) => {
-            BrowseTheWeb.as(actor).wait(this.condition, this.waitCondition as SppWebElementFinder)
+            BrowseTheWeb.as(actor).wait(this.condition, this.awaitingElement)
                 .then((message: string) => {
                     this.logger.debug(message);
                     resolve();
@@ -33,4 +28,28 @@ export class Wait implements Interaction {
                 .catch(reject)
         });
     }
+
+    /**
+     * wait until a condition is met for the given element
+     * @param awaitingElement the elements to wait for
+     */
+    public static for(awaitingElement: SppWebElementFinder) {
+        return new Wait(awaitingElement);
+    }
+
+    /**
+     * specify the condition to wait for of the given element
+     * @param condition the condition to be waiting for
+     */
+    public andCheck(condition: UntilElementCondition) {
+        this.condition = condition;
+        return this;
+    }
+
+    /**
+     * @ignore
+     */
+    constructor(private awaitingElement: SppWebElementFinder) {}
+
+
 }
