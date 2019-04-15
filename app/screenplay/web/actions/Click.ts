a7958d5093249139213ea5df97fb83e49eac6f22
Andy Schulz <andy.schulz@test-steps.de>
2019-04-15 06:08:30 +0200

    add additional documentation

--- app/screenplay/web/actions/Click.ts
+++ app/screenplay/web/actions/Click.ts
@@ -1,20 +1,33 @@
-import {BrowseTheWeb, Interaction, PerformsTask, SppWebElementFinder} from "../../../index";
-import {step, stepDetails}                                            from "../../lib/decorators/StepDecorators";
-import {SppWebElementListFinder}                                      from "../SppWebElements";
-import {UsesAbilities}                                                from "../../Actor";
+/**
+ * Action to click on a web element
+ */
+
+import {BrowseTheWeb, Interaction, SppWebElementFinder} from "../../../index";
+import {stepDetails}                                    from "../../lib/decorators/StepDecorators";
+import {SppWebElementListFinder}                        from "../SppWebElements";
+import {UsesAbilities}                                  from "../../Actor";
 
 export class Click implements Interaction {
 
+    /**
+     * @ignore
+     */
+    @stepDetails<UsesAbilities>(`click on element: '<<element>>'`)
+    performAs(actor: UsesAbilities): Promise<void> {
+        return BrowseTheWeb.as(actor).findElement(this.element).click();
+    }
 
+    /**
+     * specify which element should be clicked on
+     * @param element - the SPP Element
+     */
     public static on(element: SppWebElementFinder | SppWebElementListFinder): Click {
         return new Click(<SppWebElementFinder>element);
     }
 
+    /**
+     * @ignore
+     */
     constructor(public element: SppWebElementFinder) {
-    }
-
-    @stepDetails<UsesAbilities>(`click on element: '<<element>>'`)
-    performAs(actor: UsesAbilities): Promise<void> {
-        return BrowseTheWeb.as(actor).findElement(this.element).click();
     }
 }
