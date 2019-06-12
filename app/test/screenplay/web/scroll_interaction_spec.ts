5d9be0eb4f51aceba697fcdd91d1ad1f4009d5d4
Andy Schulz <andy.schulz@test-steps.de>
2019-06-12 15:43:49 +0200

    bugfix race condition in test

--- app/test/screenplay/web/scroll_interaction_spec.ts
+++ app/test/screenplay/web/scroll_interaction_spec.ts
@@ -7,11 +7,12 @@ import {
     BrowseTheWeb,
     RunningBrowser,
     Navigate, element, Scroll, Browser, Page
-}                                                from "../../../index";
+} from "../../../index";
 import {BoundaryCheck, boundingRect, clientRect} from "../../0_helper/browser_viewport";
 
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+
 describe(`Scroll`, (): void => {
-    let theBrowser: Browser;
 
     const lastTableRow = element(By.css(`[data-test-id='lastTableRow']`))
         .shallWait(UntilElement.is.visible().forAsLongAs(5000))
@@ -27,15 +28,18 @@ describe(`Scroll`, (): void => {
         browserName: `chrome`,
     };
 
-    const Sam = Actor.named(`Sam`);
 
-    beforeAll((): void => {
-        // and give him the ability to browse the web using a browser of your choice
-        theBrowser = RunningBrowser.startedOn(conf).withDesiredCapability(capabilities);
-        Sam.whoCan(BrowseTheWeb.using(theBrowser));
-    });
 
     describe(`to a pages position`, (): void => {
+        let theBrowser: Browser;
+        const Sam = Actor.named(`Sam`);
+
+        beforeAll((): void => {
+            // and give him the ability to browse the web using a browser of your choice
+            theBrowser = RunningBrowser.startedOn(conf).withDesiredCapability(capabilities);
+            Sam.whoCan(BrowseTheWeb.using(theBrowser));
+        });
+
         it(`should succeed when scrolled to the end of the page 
         - (test case id: 8fc292fe-883d-48ce-878e-11fcdff579df)`, async (): Promise<void> => {
             await Navigate.to(`https://teststepsframeworktester.azurewebsites.net/tables`).performAs(Sam);
@@ -55,12 +59,19 @@ describe(`Scroll`, (): void => {
             const rectStart2: ClientRectList = await theBrowser.executeScript(clientRect) as ClientRectList;
             expect(rectStart2[0].top).toBe(0);
             expect(rectStart2[0].bottom).toBeGreaterThanOrEqual(7000);
-
-            console.log(`test`);
         });
     });
 
     describe(`an element into view`, (): void => {
+        let theBrowser: Browser;
+        const Sam = Actor.named(`Sam`);
+
+        beforeAll((): void => {
+            // and give him the ability to browse the web using a browser of your choice
+            theBrowser = RunningBrowser.startedOn(conf).withDesiredCapability(capabilities);
+            Sam.whoCan(BrowseTheWeb.using(theBrowser));
+        });
+
         it(`should move the element into the viewport 
         - (test case id: bc7ff4ef-d0ea-4ac1-b2c6-5cedefd11391)`, async (): Promise<void> => {
 
@@ -72,17 +83,16 @@ describe(`Scroll`, (): void => {
 
             expect(isOutsideViewOnFirstCheck.any).toBeTruthy();
 
-            await Scroll.to(lastTableRow).performAs(Sam)
+            return Scroll.to(lastTableRow).performAs(Sam)
                 .then(async (): Promise<void> => {
                     const isOutsideViewOnSecondCheck: BoundaryCheck =
                         await theBrowser.executeScript(boundingRect, classNameOfLastRow) as BoundaryCheck;
                     expect(isOutsideViewOnSecondCheck.any).toBeFalsy()
                 })
         });
-
-
-        afterAll((): Promise<void[][]> => {
-            return RunningBrowser.cleanup();
-        })
     });
+
+    afterAll((): Promise<void[][]> => {
+        return RunningBrowser.cleanup();
+    })
 });
