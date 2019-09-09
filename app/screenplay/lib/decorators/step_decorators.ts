6d96fbc3243a717507a845fdf0aaea3a9abb8e31
Andy Schulz <andy.schulz@test-steps.de>
2019-09-09 15:49:00 +0200

    refactor activity decorators

--- app/screenplay/lib/decorators/step_decorators.ts
+++ app/screenplay/lib/decorators/step_decorators.ts
@@ -1,40 +1,39 @@
-import {getLogger, Logger} from "@log4js-node/log4js-api";
-import {Activity}          from "../../..";
-import {LogsActivity}      from "../../Actor";
-import {stringReplace}     from "./decoratorStrings";
+import {getLogger, Logger}    from "@log4js-node/log4js-api";
+import {Activity}             from "../../..";
+import {ActivityLogEntryType} from "../../../packages/ActivityLog/ActivityLogEntry";
+import {LogsActivity}         from "../../Actor";
+import {stringReplace}        from "./decoratorStrings";
 
-export function stepDetails<U extends LogsActivity, PT, RT>(text: string): (
-    target: Activity<PT, RT>,
-    propertyName: string,
-    propertyDesciptor: TypedPropertyDescriptor<((actor: U, param?: PT) => Promise<RT>)>) => PropertyDescriptor {
-
+const createLogTask = <U extends LogsActivity, PT, RT>(
+    description: string,
+    activityDetails: string,
+    activityType: ActivityLogEntryType) => {
     return function logTask(
         target: Activity<PT, RT>,
         propertyName: string,
         propertyDesciptor: TypedPropertyDescriptor<((actor: U, param?: PT) => Promise<RT>)>): PropertyDescriptor {
         const logger: Logger = getLogger(target.constructor.name);
-
         const method = propertyDesciptor.value as () => Promise<RT>;
 
-        target.toString = stringReplace(text);
+        target.toString = stringReplace(description);
 
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         // propertyDesciptor.value = function (...args: any): Promise<RT> {
         propertyDesciptor.value = function (actor: U, param?: PT): Promise<RT> {
 
             const logEntry = actor.activityLog.addActivityLogEntry(target.constructor.name,
-                `${actor.name} attempts to ${this.toString()}`,
-                `Interaction`);
+                `${actor.name} ${activityDetails} ${this.toString()}`,
+                activityType);
             // @ts-ignore
             if (typeof logger.stepdetails == `function`) {
                 // @ts-ignore
-                logger.stepdetails(`${actor.name} attempts to ${this.toString()}`);
+                logger.stepdetails(`${actor.name} ${activityDetails} ${this.toString()}`);
             } else {
-                logger.debug(`${actor.name} attempts to ${this.toString()}`);
+                logger.debug(`${actor.name} ${activityDetails} ${this.toString()}`);
             }
             const arg = [actor, param];
             return method.apply(this, arg as [])
-                .then((value: RT) => {
+                .then((value: RT): RT => {
                     actor.activityLog.reset(logEntry);
                     return value;
                 });
@@ -42,6 +41,14 @@ export function stepDetails<U extends LogsActivity, PT, RT>(text: string): (
 
         return propertyDesciptor;
     }
+};
+
+export function stepDetails<U extends LogsActivity, PT, RT>(text: string): (
+    target: Activity<PT, RT>,
+    propertyName: string,
+    propertyDesciptor: TypedPropertyDescriptor<((actor: U, param?: PT) => Promise<RT>)>) => PropertyDescriptor {
+
+    return createLogTask(text, `attempts to`, `Interaction`)
 }
 
 export function skip<U extends LogsActivity, PT, RT>(text: string): (
@@ -49,41 +56,7 @@ export function skip<U extends LogsActivity, PT, RT>(text: string): (
     propertyName: string,
     propertyDesciptor: TypedPropertyDescriptor<((actor: U, param?: PT) => Promise<RT>)>) => PropertyDescriptor {
 
-    return function logTask(
-        target: Activity<PT, RT>,
-        propertyName: string,
-        propertyDesciptor: TypedPropertyDescriptor<((actor: U, param?: PT) => Promise<RT>)>): PropertyDescriptor {
-        const logger: Logger = getLogger(target.constructor.name);
-
-        const method = propertyDesciptor.value as () => Promise<RT>;
-
-        target.toString = stringReplace(text);
-
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        // propertyDesciptor.value = function (...args: any): Promise<RT> {
-        propertyDesciptor.value = function (actor: U, param?: PT): Promise<RT> {
-            const logEntry = actor.activityLog.addActivityLogEntry(
-                target.constructor.name,
-                `${actor.name} skips ${this.toString()}`,
-                `Task`
-            );
-            // @ts-ignore
-            if (typeof logger.stepdetails == `function`) {
-                // @ts-ignore
-                logger.stepdetails(`${actor.name} skips ${this.toString()}`);
-            } else {
-                logger.debug(`${actor.name} skips ${this.toString()}`);
-            }
-            const args = [actor, param];
-            return method.apply(this, args as  [])
-                .then((value: RT) => {
-                    actor.activityLog.reset(logEntry);
-                    return value;
-                });
-        };
-
-        return propertyDesciptor;
-    }
+    return createLogTask(text, `skips`, `Task`);
 }
 
 export function step<U extends LogsActivity, PT, RT>(text: string): (
@@ -91,34 +64,5 @@ export function step<U extends LogsActivity, PT, RT>(text: string): (
     propertyName: string,
     propertyDesciptor: TypedPropertyDescriptor<((actor: U, param?: PT) => Promise<RT>)>) => PropertyDescriptor {
 
-    return function logTask(
-        target: Activity<PT, RT>,
-        propertyName: string,
-        propertyDesciptor: TypedPropertyDescriptor<((actor: U, param?: PT) => Promise<RT>)>): PropertyDescriptor {
-        const logger = getLogger(target.constructor.name);
-
-        const method = propertyDesciptor.value as () => Promise<RT>;
-
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        // propertyDesciptor.value = function (...args: any): Promise<RT> {
-        propertyDesciptor.value = function (actor: U, param?: PT): Promise<RT> {
-            const logEntry = actor.activityLog.addActivityLogEntry(target.constructor.name,
-                `${actor.name} attempts to ${text}`,
-                `Task`);
-            // @ts-ignore
-            if (typeof logger.step == `function`) {
-                // @ts-ignore
-                logger.step(`${actor.name} attempts to ${text}`);
-            } else {
-                logger.debug(`${actor.name} attempts to ${text}`);
-            }
-            const args = [actor, param];
-            return method.apply(this, args as [])
-                .then((value: RT) => {
-                    actor.activityLog.reset(logEntry);
-                    return value;
-                });
-        };
-        return propertyDesciptor;
-    }
+    return createLogTask(text, `attempts to`, `Task`);
 }
