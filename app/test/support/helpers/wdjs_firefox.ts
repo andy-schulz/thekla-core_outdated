7eaf4ead4f5c97084690fafafcf523293c222941
Andy Schulz <andy.schulz@test-steps.de>
2019-09-24 12:16:26 +0200

    remove wdjs support files

--- app/test/support/helpers/wdjs_firefox.ts
+++
@@ -1,5 +0,0 @@
-process.env.FRAMEWORK = `wdjs`;
-process.env.BROWSERNAME=`firefox`;
-console.log(`STARTING with FRAMEWORK:${process.env.FRAMEWORK}`);
-console.log(`STARTING with BROWSERNAME:${process.env.BROWSERNAME}`);
-
