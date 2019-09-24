d83faa8fd68b8665bd320d076cdd424996683f9b
Andy Schulz <andy.schulz@test-steps.de>
2019-09-24 12:11:58 +0200

    add reason to Sleep interaction

--- app/screenplay/lib/actions/Sleep.ts
+++ app/screenplay/lib/actions/Sleep.ts
@@ -6,15 +6,9 @@ import {stepDetails}   from "../decorators/step_decorators";
 
 export class Sleep implements Interaction<void, void> {
     private logger = getLogger(`Sleep`);
+    public sleepReason: string = ``;
 
-    public static for(sleepTime: number): Sleep {
-        return new Sleep(sleepTime);
-    }
-
-    private constructor(private sleepTime: number) {
-    }
-
-    @stepDetails<UsesAbilities, void, void>(`stop all actions for '<<sleepTime>>' ms`)
+    @stepDetails<UsesAbilities, void, void>(`stop all actions for '<<sleepTime>>' ms<<sleepReason>>`)
     // parameter is needed for stepDetails typing
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     public performAs(actor: UsesAbilities): Promise<void> {
@@ -22,4 +16,17 @@ export class Sleep implements Interaction<void, void> {
             return this.logger.trace(`Slept for ${this.sleepTime}`);
         });
     }
+
+    public static for(sleepTime: number): Sleep {
+        return new Sleep(sleepTime);
+    }
+
+    public because(sleepReason: string): Sleep {
+        this.sleepReason = ` because ${sleepReason}`;
+        return this;
+    }
+
+    private constructor(private sleepTime: number) {
+    }
+
 }
